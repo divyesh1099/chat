73708c264feb0991f01ec09526f6314f0c516660
Divyesh <divyesh1099@gmail.com>
2020-12-29 11:41:54 +0530

    Simple Chat app using Web Sockets

---
+++ core/routing.py
@@ -0,0 +1,13 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing 
+from channels.middleware import BaseMiddleware
+from django.core.asgi import get_asgi_application
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
